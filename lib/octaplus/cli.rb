5babfed143fb0346c66bda9374ceb1abbc09e5f0
Jeroen Bourgois <jeroenbourgois@gmail.com>
2012-10-01 23:07:24 +0200

    working first version

--- lib/octaplus/cli.rb
+++ lib/octaplus/cli.rb
@@ -5,18 +5,38 @@ module Octaplus
   class CLI < Thor
     desc "init PROJECT", "Creates a new Octaplus project, bare"
     def init(project)
-      puts "#{project}"
+      puts "⚡ Preparing your package, hold on ⚡", ""
+
+      # save pwd
+      pwd = Dir.pwd
+
+      # remove previous
+      %x[rm -rf /tmp/proxdev]
+
+      # create some collecting folders
+      # and move to the root of them
+      %x[mkdir -p /tmp/proxdev/octaplus /tmp/proxdev/bearded /tmp/proxdev/boilerplate]
+
+      Dir.chdir "/tmp/proxdev"
 
       # get the octaplus framework
-      %x[git clone https://github.com/proximitybbdo/octaplus /tmp/proxdev/octaplus_tmp]
+      puts '   ⚡ A bit of Octaplus goodness'
+      %x[curl -L -s https://github.com/proximitybbdo/octaplus/tarball/master | tar xz --strip 1 -C octaplus]
 
-      # # get the bearded octo
-      %x[git clone https://github.com/proximitybbdo/octaplus /tmp/proxdev/bearded_tmp]
+      # get the bearded octo
+      puts '   ⚡ And some bearded-octo seasoning'
+      %x[curl -L -s https://github.com/proximitybbdo/bearded-octo/tarball/master | tar xz --strip 1 -C bearded]
+
+      # merge the two into boilerplate goodness
+      puts '   ⚡ All in the boilerplate'
+      %x[cp -R octaplus/ boilerplate]
+      %x[rm -rf -R boilerplate/public/assets]
+      %x[cp -R bearded/assets boilerplate/public]
 
       # copy the bundle to the current working directory
-      pwd = Dir.pwd
-      Dir.chdir "/tmp/proxdev/octaplus_tmp"
-      %x[mkdir -p #{pwd}/octaplus | git archive master | tar -x -C #{pwd + '/octaplus'}]
+      puts "", "⚡⚡⚡ Et voila! All set in the ./#{project} folder ⚡⚡⚡ ", ""
+
+      %x[mkdir -p #{pwd}/#{project} | cp -R boilerplate #{pwd}/#{project}]
     end
 
   end

d7ebb1c8294cacb893797678542328f35a7115e4
Jeroen Bourgois <jeroenbourgois@gmail.com>
2012-10-03 16:17:04 +0200

    forgot some files

---
+++ lib/carb/config.rb
@@ -0,0 +1,9 @@
+module Carb
+  TYPE_OCTAPLUS = 'octaplus'
+  TYPE_BEARDED_OCTO = 'bearded-octo'
+  TYPE_ON_FIRE = 'on-fire'
+
+  PROJECT_TYPES = [TYPE_OCTAPLUS, TYPE_BEARDED_OCTO, TYPE_ON_FIRE]
+
+  TMP_FOLDER = "/tmp/proxdev"
+end
